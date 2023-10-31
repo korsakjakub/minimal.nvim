6faa5683f3ec78a1149e9d84c51a21ebd485c1e6
Jakub Korsak <jk@Jakubs-MacBook-Air.local>
2023-10-31 22:06:04 +0100

    adding kickstart tmpl

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,7 +1,7 @@
 local config = {
     cmd = {
 	'java',
-    '-javaagent:/home/jakub/.local/lib/lombok.jar',
+    '-javaagent:/Users/jk/.local/lib/lombok.jar',
 	'-Declipse.application=org.eclipse.jdt.ls.core.id1',
 	'-Dosgi.bundles.defaultStartLevel=4',
 	'-Declipse.product=org.eclipse.jdt.ls.core.product',
@@ -11,11 +11,34 @@ local config = {
 	'--add-modules=ALL-SYSTEM',
 	'--add-opens', 'java.base/java.util=ALL-UNNAMED',
 	'--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-	'-jar', '/home/jakub/.local/bin/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
-	'-configuration', '/home/jakub/.local/bin/jdtls/config_linux',
-	'-data', '/home/jakub/.cache/jdtls_workspace' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+	'-jar', '/Users/jk/.local/bin/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
+	'-configuration', '/Users/jk/.local/bin/jdtls/config_mac',
+	'-data', '/Users/jk/.cache/jdtls_workspace' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
     },
     root_dir = vim.fs.dirname(vim.fs.find({'pom.xml', '.git', 'mvnw'}, { upward = true })[1]),
+    settings = {
+        java = {
+            import = {enabled = true},
+            rename = {enabled = true},
+            signatureHelp = { enabled = true },
+            completion = {
+                favoriteStaticMembers = {},
+                filteredTypes = {
+                    -- "com.sun.*",
+                    -- "io.micrometer.shaded.*",
+                    -- "java.awt.*",
+                    -- "jdk.*",
+                    -- "sun.*",
+                },
+            },
+            sources = {
+                organizeImports = {
+                    starThreshold = 9999,
+                    staticStarThreshold = 9999,
+                },
+            },
+        },
+    },
 }
 
 
