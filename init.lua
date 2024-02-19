44edb1ce3a6abddb3f93e2b0d5afc22bfd72e087
Jakub Korsak <jakub.korsak@rba.hr>
2024-02-19 09:59:09 +0100

    spectre

--- init.lua
+++ init.lua
@@ -205,6 +205,10 @@ require('lazy').setup({
   },
 
   {
+    'nvim-pack/nvim-spectre'
+  },
+
+  {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
@@ -349,6 +353,20 @@ vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iag
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+-- Configure Spectre
+vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
+  desc = "Toggle Spectre"
+})
+vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+  desc = "Search current word"
+})
+vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+  desc = "Search current word"
+})
+vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+  desc = "Search on current file"
+})
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
