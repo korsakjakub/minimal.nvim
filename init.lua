b047341c274a0b07607225e7253f91ec94f69ecf
Jakub Korsak <jakub@korsak.xyz>
2024-02-25 18:36:12 +0100

    rm go plugin, leave gopls

--- init.lua
+++ init.lua
@@ -57,20 +57,6 @@ require('lazy').setup({
     branch = 'v3.x',
     lazy = true,
   },
-  {
-    "ray-x/go.nvim",
-    dependencies = { -- optional packages
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("go").setup()
-    end,
-    event = { "CmdlineEnter" },
-    ft = { "go", 'gomod' },
-    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-  },
 
   {
     -- LSP Configuration & Plugins
@@ -625,15 +611,11 @@ cmp.setup {
   },
 }
 
-local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    require('go.format').goimport()
-  end,
-  group = format_sync_grp,
+vim.filetype.add({
+  extension = {
+    templ = "templ",
+  },
 })
-require('go').setup()
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
